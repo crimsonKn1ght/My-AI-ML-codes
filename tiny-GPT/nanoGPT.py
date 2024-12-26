68cc6c3b32ab42088600a0fa14cbb05b401b5fcf
Gourab Roy <87464911+crimsonKn1ght@users.noreply.github.com>
2024-12-26 22:58:19 +0530

    Update nanoGPT.py

--- tiny-GPT/nanoGPT.py
+++ tiny-GPT/nanoGPT.py
@@ -78,3 +78,51 @@ class bigramLanguageModel(nn.Module):
 m = bigramLanguageModel(vocab_size)
 out = m(xb, yb)
 print(out.shape)
+
+optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
+
+batch_size = 32
+for steps in range(100): # increase number of steps for good results...
+
+    # sample a batch of data
+    xb, yb = get_batch('train')
+
+    # evaluate the loss
+    logits, loss = m(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+print(loss.item())
+
+print(decode(m.generate(idx = torch.zeros((1, 1), dtype=torch.long), max_new_tokens=500)[0].tolist()))
+
+torch.manual_seed(42)
+a = torch.tril(torch.ones(3, 3))
+a = a / torch.sum(a, 1, keepdim=True)
+b = torch.randint(0,10,(3,2)).float()
+c = a @ b
+print('a=')
+print(a)
+print('--')
+print('b=')
+print(b)
+print('--')
+print('c=')
+print(c)
+
+torch.manual_seed(1337)
+B,T,C = 4,8,2
+x = torch.randn(B,T,C)
+x.shape
+
+xbow = torch.zeros((B,T,C))
+for b in range(B):
+    for t in range(T):
+        xprev = x[b,:t+1] # (t,C)
+        xbow[b,t] = torch.mean(xprev, 0)
+
+wei = torch.tril(torch.ones(T, T))
+wei = wei / wei.sum(1, keepdim=True)
+xbow2 = wei @ x # (B, T, T) @ (B, T, C) ----> (B, T, C)
+torch.allclose(xbow, xbow2)
