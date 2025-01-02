9fd98248b9bf228c8c962c8e19bf1312283ffe64
Gourab Roy <87464911+crimsonKn1ght@users.noreply.github.com>
2025-01-02 20:27:40 +0530

    Update unet.py
    Segmentation issue remains

--- U-Net [my implementation]/unet.py
+++ U-Net [my implementation]/unet.py
@@ -16,144 +16,102 @@ if device == 'cuda':
 else:
     print("Cuda unavailable.")
 
-# Defining the basic conv block of U-Net
-class Conv2D(nn.Module):
-    def __init__(self, in_c, out_c, kernel_size=3, padding=0, act=True):
+# Defining the downsampling and upsampling conv block of U-Net
+class downConv(nn.Module):
+    def __init__(self, in_c, out_c, kernel_size=3, padding=0, stride=1, act=True):
         super().__init__()
 
-        self.conv2d = nn.Sequential(
-                nn.Conv2d(
-                in_c,
-                out_c,
-                kernel_size=kernel_size,
-                padding=padding,
-            ),
-            nn.BatchNorm2d(out_c)
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_c, out_c, kernel_size=kernel_size, padding=padding, stride=stride,),
+            nn.BatchNorm2d(out_c),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_c, out_c, kernel_size=kernel_size, padding=padding, stride=stride,),
+            nn.BatchNorm2d(out_c),
+            nn.ReLU(inplace=True),
         )
 
-        self.relu = nn.ReLU(inplace=True)
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class upConv(nn.Module):
+    def __init__(self, in_c, out_c, kernel_size=3, padding=0, stride=1, act=True):
+        super().__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_c, in_c // 2, kernel_size=kernel_size, padding=padding, stride=stride,),
+            nn.BatchNorm2d(out_c),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_c // 2, out_c, kernel_size=kernel_size, padding=padding, stride=stride,),
+            nn.BatchNorm2d(out_c),
+            nn.ReLU(inplace=True),
+        )
 
     def forward(self, x):
-        x = self.conv2d(x)
-        
-        if act==True:
-            x = self.relu(x)
-
+        x = self.conv(x)
         return x
 
 
 # Define U-Net
 
-class unet_first(nn.Module):
+class unet_encoder(nn.Module):
     def __init__(self):
         super().__init__()
 
         self.block1 = nn.Sequential(
-            Conv2D(in_c=1, out_c=64, kernel_size=3),
-            Conv2D(in_c=64, out_c=64, kernel_size=3),
-            Conv2D(in_c=64, out_c=64, kernel_size=3),
+            downConv(in_c=1, out_c=64, kernel_size=3, padding=0, stride=1),
         )
 
         self.block2 = nn.Sequential(
-            Conv2D(in_c=64, out_c=128, kernel_size=3),
-            Conv2D(in_c=128, out_c=128, kernel_size=3),
-            Conv2D(in_c=128, out_c=128, kernel_size=3),
+            nn.MaxPool2d(2),
+            downConv(in_c=64, out_c=128, kernel_size=3, padding=0, stride=1),
         )
 
         self.block3 = nn.Sequential(
-            Conv2D(in_c=128, out_c=256, kernel_size=3),
-            Conv2D(in_c=256, out_c=256, kernel_size=3),
-            Conv2D(in_c=256, out_c=256, kernel_size=3),
+            nn.MaxPool2d(2),
+            downConv(in_c=128, out_c=256, kernel_size=3, padding=0, stride=1),
         )
 
         self.block4 = nn.Sequential(
-            Conv2D(in_c=256, out_c=512, kernel_size=3),
-            Conv2D(in_c=512, out_c=512, kernel_size=3),
-            Conv2D(in_c=512, out_c=512, kernel_size=3),
+            nn.MaxPool2d(2),
+            downConv(in_c=256, out_c=512, kernel_size=3, padding=0, stride=1),
         )
 
         self.block5 = nn.Sequential(
-            Conv2D(in_c=512, out_c=1024, kernel_size=3),
-            Conv2D(in_c=1024, out_c=1024, kernel_size=3),
-            Conv2D(in_c=1024, out_c=1024, kernel_size=3),
+            nn.MaxPool2d(2),
+            downConv(in_c=512, out_c=1024, kernel_size=3, padding=0, stride=1),
         )
 
 
     def forward(self, x):
         x1 = self.block1(x)
-        x1p = nn.MaxPool2d(x1)
-        
-        x2 = self.block1(x1p)
-        x2p = nn.MaxPool2d(x2)
-        
-        x3 = self.block1(x2p)
-        x3p = nn.MaxPool2d(x3)
-        
-        x4 = self.block1(x3p)
-        x4p = nn.MaxPool2d(x4)
-        
-        x5 = self.block1(x4)
+        x2 = self.block2(x1)
+        x3 = self.block3(x2)
+        x4 = self.block4(x3)
+        x5 = self.block5(x4)
 
         return [x1, x2, x3, x4, x5]
 
 
-class unet_second(nn.Module):
+class unet_decoder(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.upconv1 = nn.ConvTranspose2d(
-            in_channels=1024,
-            out_channels=512,
-            kernel_size=2,
-            stride=2
-        )
-
-        self.upconv2 = nn.ConvTranspose2d(
-            in_channels=512,
-            out_channels=256,
-            kernel_size=2,
-            stride=2
-        )
-
-        self.upconv3 = nn.ConvTranspose2d(
-            in_channels=256,
-            out_channels=128,
-            kernel_size=2,
-            stride=2
-        )
-
-        self.upconv4 = nn.ConvTranspose2d(
-            in_channels=128,
-            out_channels=64,
-            kernel_size=2,
-            stride=2
-        )
+        self.upconv1 = nn.ConvTranspose2d(in_channels=1024, out_channels=512, kernel_size=2, stride=2,)
+        self.upconv2 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=2, stride=2,)
+        self.upconv3 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=2, stride=2,)
+        self.upconv4 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=2, stride=2,)
         
-        self.block1 = nn.Sequential(
-            Conv2D(in_c=1024, out_c=512, kernel_size=3),
-            Conv2D(in_c=512, out_c=512, kernel_size=3),
-            Conv2D(in_c=512, out_c=512, kernel_size=3),
+        self.block1 = upConv(in_c=1024, out_c=256, kernel_size=3, padding=0, stride=1)
+        self.block2 = upConv(in_c=512, out_c=128, kernel_size=3, padding=0, stride=1)
+        self.block3 = upConv(in_c=256, out_c=64, kernel_size=3, padding=0, stride=1)
+        self.block4 = upConv(in_c=128, out_c=64, kernel_size=3, padding=0, stride=1)
+        
+        self.seg_head = nn.Sequential(
+            nn.Conv2d(64, 2, kernel_size=3, padding=1, stride=1,),
+            nn.Sigmoid(2)
         )
-
-        self.block2 = nn.Sequential(
-            Conv2D(in_c=512, out_c=256, kernel_size=3),
-            Conv2D(in_c=256, out_c=256, kernel_size=3),
-            Conv2D(in_c=256, out_c=256, kernel_size=3),
-        )
-
-        self.block3 = nn.Sequential(
-            Conv2D(in_c=256, out_c=128, kernel_size=3),
-            Conv2D(in_c=128, out_c=128, kernel_size=3),
-            Conv2D(in_c=128, out_c=128, kernel_size=3),
-        )
-
-        self.block4 = nn.Sequential(
-            Conv2D(in_c=128, out_c=64, kernel_size=3),
-            Conv2D(in_c=64, out_c=64, kernel_size=3),
-            Conv2D(in_c=64, out_c=64, kernel_size=3),
-        )
-
-        self.lasthead = Conv2D(in_c=64, out_c=2, padding=1, stride=1)
 
     def forward(self, x_set):
         x1_1, x1_2, x1_3, x1_4, x1_5 = x_set
@@ -174,7 +132,20 @@ class unet_second(nn.Module):
         x2_4 = torch.cat([x2_4, x1_1], axis=1)
         x2_4 = nn.block1(x2_4)
 
-        x = self.lasthead(x2_4)
+        x = self.seg_head(x2_4)
         
         return x
-        
+
+
+class u_net(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.part1 = unet_encoder()
+        self.part2 = unet_decoder()
+
+    def forward(self, x):
+        x1 = self.part1(x)
+        x2 = self.part2(x1)
+
+        return x
